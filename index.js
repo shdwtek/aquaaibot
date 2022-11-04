6e614783e3d9a1b63f2e8352b6ff48df8b8d6ed3
Evan Coury <me@evancoury.com>
2022-11-04 14:15:47 -0400

    Add water temp awareness

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const tmi = require('tmi.js');
 const generator = require('./textGenerator');
+const http = require('http');
 
 const reputation = {};
+
+function doRequest(url) {
+  return new Promise(function (resolve, reject) {
+    http.get(url, function(response) {
+      var data = '';
+      response.on('data', function (chunk) {
+          data += chunk;
+      });
+      response.on('end', async function () {
+        resolve(data);
+      });
+      response.on('error', async function (error) {
+        reject(error);
+      });
+    });
+  });
+}
+
 const client = new tmi.Client({
   options: { debug: true },
   connection: {
@@ -27,9 +46,12 @@ client.on('message', (channel, tags, message, self) => {
 
   if(command === process.env.TWITCH_BOT_USERNAME) {
     (async () => {
+      var tankTemp = await doRequest('http://shdwtek.net/temp.html');
+      tankTemp = tankTemp.replace(/<[^>]+>/g, ' ').trim().replace(/ +/, ' ');
       var promptText = process.env.OPENAI_PROMPT
         .replace(/\{botname\}/g, tags['display-name'])
-        .replace('{message}', args.join(' ')).trim();
+        .replace('{message}', args.join(' ')).trim()
+        .replace('{temp}', tankTemp + '°F');
       // Add a period if necessary so the bot doesn't try to complete the prompt.
       if (!['.','?'].includes(promptText.slice(-1))) {
         promptText = `${promptText}.}`;
