83c95ac8d157cddde7cddb83f81f79ecc6b23853
Evan Coury <me@evancoury.com>
2022-11-18 17:43:05 -0500

    Try three times for a non-conversational response

--- textGenerator.js
+++ textGenerator.js
@@ -14,6 +14,39 @@ async function generate(promptText) {
     temperature: parseFloat(process.env.OPENAI_TEMPERATURE),
     frequency_penalty: parseFloat(process.env.OPENAI_FREQ_PENALTY),
   });
+
+  // Try 3 times to get a non-conversational response...
+  if (completion.data.choices[0].text.match(/:.+:/g)) {
+    console.log('OpenAI being dumb...');
+    const completion = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL,
+      prompt: promptText,
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS),
+      temperature: parseFloat(process.env.OPENAI_TEMPERATURE),
+      frequency_penalty: parseFloat(process.env.OPENAI_FREQ_PENALTY),
+    });
+  }
+  if (completion.data.choices[0].text.match(/:.+:/g)) {
+    console.log('OpenAI being dumb...');
+    const completion = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL,
+      prompt: promptText,
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS),
+      temperature: parseFloat(process.env.OPENAI_TEMPERATURE),
+      frequency_penalty: parseFloat(process.env.OPENAI_FREQ_PENALTY),
+    });
+  }
+  if (completion.data.choices[0].text.match(/:.+:/g)) {
+    console.log('OpenAI being dumb...');
+    const completion = await openai.createCompletion({
+      model: process.env.OPENAI_MODEL,
+      prompt: promptText,
+      max_tokens: parseInt(process.env.OPENAI_MAX_TOKENS),
+      temperature: parseFloat(process.env.OPENAI_TEMPERATURE),
+      frequency_penalty: parseFloat(process.env.OPENAI_FREQ_PENALTY),
+    });
+  }
+
   return completion.data.choices[0].text;
 }
 
